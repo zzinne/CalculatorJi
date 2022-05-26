1cd5618ffc351397c5cf8ac1d69701278df0ad44
zzinne <zzinee1005@naver.com>
2022-05-26 23:06:10 +0900

    계산기 프로젝트 커밋 1. 문자열 사이즈 체크 2. 문자열에 연사기호를 제외한 특수문자 포함되어있는지 체크

--- src/main/java/Validation.java
+++ src/main/java/Validation.java
@@ -1,8 +1,16 @@
+import java.util.regex.Pattern;
+
 public class Validation {
     public void trimTextSizeCheck(String inputText){
         if(inputText.trim()
                     .length() == 0){
             throw new IllegalArgumentException("문자가 입력되지 않았습니다.");
+        }
+    }
+
+    public void textRegexCheck(String inputText){
+        if(!Pattern.matches("^[0-9 \\-+*/]$",inputText)){
+            throw new IllegalArgumentException("연산기호를 제외한 특수기호가 포함되어있습니다.");
         }
     }
 
