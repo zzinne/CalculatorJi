a9443afe23818e49398e73a96747e867b29eddd7
zzinne <zzinee1005@naver.com>
2022-05-29 22:46:07 +0900

    계산기 프로젝트 커밋 1. 벨리데이션 체크부분 수정

--- src/main/java/Validation.java
+++ src/main/java/Validation.java
@@ -9,7 +9,7 @@ public class Validation {
     }
 
     public void textRegexCheck(String inputText){
-        if(!Pattern.matches("^[0-9 \\-+*/]$",inputText)){
+        if(!Pattern.matches("^[0-9 \\-+*/]*$",inputText)){
             throw new IllegalArgumentException("연산기호를 제외한 특수기호가 포함되어있습니다.");
         }
     }
